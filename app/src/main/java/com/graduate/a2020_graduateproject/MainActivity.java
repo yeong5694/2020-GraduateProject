7e19f5f1566c4cfe91813e00b25baedec6a0d225
hsy5694 <hsy5694@naver.com>
2020-03-07 15:56:56 +0900

    2020-03-07  create empty project

---
+++ app/src/main/java/com/graduate/a2020_graduateproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.graduate.a2020_graduateproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
