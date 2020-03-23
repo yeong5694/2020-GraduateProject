f04036e7d0ed8a10ef80d01124ba13ce9c4b81a3
hsy5694 <hsy5694@naver.com>
2020-03-23 20:37:33 +0900

    2020-03-23  지도에서 장소 입력하면 그 장소(파란색 표시)와 장소 주변 음식점(빨간색 표시) 나타남

---
+++ app/src/main/java/com/graduate/a2020_graduateproject/KakaoTalkMainActivity.java
@@ -0,0 +1,88 @@
+package com.graduate.a2020_graduateproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.kakao.kakaotalk.callback.TalkResponseCallback;
+import com.kakao.kakaotalk.response.KakaoTalkProfile;
+import com.kakao.kakaotalk.v2.KakaoTalkService;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.util.helper.log.Logger;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class KakaoTalkMainActivity extends AppCompatActivity {
+
+    private ImageView profile_image;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+
+        profile_image = findViewById(R.id.profile_image);
+
+        requestProfile();
+
+        // 테스트
+        Glide.with(this).load("http://th-p.talk.kakao.co.kr/th/talkp/wlAvDlYuxv/aVB09m4qm7fcs4g6hLQD71/tm82pt_110x110_c.jpg")
+                .error(R.drawable.error_img).into(profile_image);
+
+    }
+
+
+    private abstract class KakaoTalkResponseCallback<T> extends TalkResponseCallback<T> {
+        @Override
+        public void onNotKakaoTalkUser() {
+            Log.e("KakaoTalkResponseCallback ::", "카카오톡 사용자가 아니다");
+        }
+
+        @Override
+        public void onFailure(ErrorResult errorResult) {
+            Log.e("KakaoTalkResponseCallback ::", "이외 다른 이유");
+            Logger.e("failure : " + errorResult);
+        }
+
+        @Override
+        public void onSessionClosed(ErrorResult errorResult) {
+            Log.e("KakaoTalkResponseCallback ::", "세션이 닫혀 실패한경우");
+            redirectLoginActivity();
+        }
+    }
+
+    public void requestProfile() {
+        KakaoTalkService.getInstance().requestProfile(new KakaoTalkResponseCallback<KakaoTalkProfile>() {
+            @Override
+            public void onSuccess(KakaoTalkProfile talkProfile) { // 실시간 톡 프로필 정보
+                Log.e("KakaoTalkResponseCallback ::", "프로필 가져오기 시작");
+
+                final String nickName = talkProfile.getNickName(); // 카톡 별명
+                final String profileImageURL = talkProfile.getProfileImageUrl(); // 카톡 프로필 이미지
+                final String thumbnailURL = talkProfile.getThumbnailUrl(); // 카톡 썸네일 이미지
+                final String countryISO = talkProfile.getCountryISO(); // 카톡 국가
+
+                Log.e("KakaoTalkProfile ::", "nickname = " + nickName);
+                Log.e("KakaoTalkProfile ::", "profileImageURL = " + profileImageURL);
+                Log.e("KakaoTalkProfile ::", "thumbnailURL = " + thumbnailURL);
+                Log.e("KakaoTalkProfile ::", "countryISO = " + countryISO);
+
+                Log.e("KakaoTalkResponseCallback ::", "프로필 가져오기 종료");
+            }
+        });
+    }
+
+    protected void redirectLoginActivity(){
+        final Intent intent = new Intent(KakaoTalkMainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/graduate/a2020_graduateproject/TripRoomActivity.java
@@ -0,0 +1,99 @@
+package com.graduate.a2020_graduateproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class TripRoomActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_room);
+        setTitle("○○ 여행");
+
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.openNavDrawer,
+                R.string.closeNavDrawer
+        );
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.home) {
+            Intent intent = new Intent(TripRoomActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+        else if(id == R.id.trip_mates) {
+
+        }
+        else if(id == R.id.chat) {
+
+        }
+        else if(id == R.id.calendar) {
+
+        }
+        else if(id == R.id.plan) {
+
+        }
+        else if(id == R.id.map) {
+            Intent intent = new Intent(TripRoomActivity.this, MapActivity.class);
+            startActivity(intent);
+        }
+        else if(id == R.id.memo) {
+
+        }
+        else if(id == R.id.gallery) {
+
+        }
+        else if(id == R.id.settings) {
+
+        }
+        else if(id == R.id.logout) {
+
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+}
