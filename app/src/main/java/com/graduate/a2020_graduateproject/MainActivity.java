16be5d35c33095a55395226b81b53ac956ac818f
sylee0129 <369256@naver.com>
2020-04-01 22:13:12 +0900

    + 이메일까지 띄우기

--- app/src/main/java/com/graduate/a2020_graduateproject/MainActivity.java
+++ app/src/main/java/com/graduate/a2020_graduateproject/MainActivity.java
@@ -9,13 +9,11 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -30,7 +28,12 @@ import com.kakao.kakaotalk.v2.KakaoTalkService;
 import com.kakao.network.ErrorResult;
 import com.kakao.usermgmt.UserManagement;
 import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
 import com.kakao.usermgmt.callback.UnLinkResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.usermgmt.response.model.Profile;
+import com.kakao.usermgmt.response.model.UserAccount;
+import com.kakao.util.OptionalBoolean;
 import com.kakao.util.helper.log.Logger;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -43,7 +46,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     View nav_header_view;
     private ImageView profile_image;
     private TextView name;
-    private TextView email;
+    private TextView kakaoEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,8 +76,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         nav_header_view = navigationView.getHeaderView(0);
         profile_image = nav_header_view.findViewById(R.id.profile_image);
         name = (TextView)nav_header_view.findViewById(R.id.name);
-        email = (TextView)nav_header_view.findViewById(R.id.email);
-        requestProfile();
+        kakaoEmail = (TextView)nav_header_view.findViewById(R.id.email);
+        requestUserInfo();
 
         // 선영이 지도 연결 버튼
 
@@ -88,24 +91,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 //        });
 
         /* 카카오 sdk 토큰 정보 가져오기*/
-        AuthService.getInstance()
-                .requestAccessTokenInfo(new ApiResponseCallback<AccessTokenInfoResponse>() {
-                    @Override
-                    public void onSessionClosed(ErrorResult errorResult) {
-                        Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
-                    }
+        requestTokenInfo();
 
-                    @Override
-                    public void onFailure(ErrorResult errorResult) {
-                        Log.e("KAKAO_API", "토큰 정보 요청 실패: " + errorResult);
-                    }
 
-                    @Override
-                    public void onSuccess(AccessTokenInfoResponse result) {
-                        Log.i("KAKAO_API", "사용자 아이디: " + result.getUserId());
-                        Log.i("KAKAO_API", "남은 시간 (ms): " + result.getExpiresInMillis());
-                    }
-                });
     }
 
     @Override
@@ -265,5 +253,83 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
             }
         });
+    }
+
+    public void requestTokenInfo(){
+        AuthService.getInstance()
+                .requestAccessTokenInfo(new ApiResponseCallback<AccessTokenInfoResponse>() {
+                    @Override
+                    public void onSessionClosed(ErrorResult errorResult) {
+                        Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
+                    }
+
+                    @Override
+                    public void onFailure(ErrorResult errorResult) {
+                        Log.e("KAKAO_API", "토큰 정보 요청 실패: " + errorResult);
+                    }
+
+                    @Override
+                    public void onSuccess(AccessTokenInfoResponse result) {
+                        Log.i("KAKAO_API", "사용자 아이디: " + result.getUserId());
+                        Log.i("KAKAO_API", "남은 시간 (ms): " + result.getExpiresInMillis());
+                    }
+                });
+    }
+
+    public void requestUserInfo(){
+        UserManagement.getInstance()
+                .me(new MeV2ResponseCallback() {
+                    @Override
+                    public void onSessionClosed(ErrorResult errorResult) {
+                        Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
+                    }
+
+                    @Override
+                    public void onFailure(ErrorResult errorResult) {
+                        Log.e("KAKAO_API", "사용자 정보 요청 실패: " + errorResult);
+                    }
+
+                    @Override
+                    public void onSuccess(MeV2Response result) {
+                        Log.i("KAKAO_API", "사용자 아이디: " + result.getId());
+
+                        UserAccount kakaoAccount = result.getKakaoAccount();
+                        if (kakaoAccount != null) {
+
+                            // 이메일
+                            String email = kakaoAccount.getEmail();
+
+                            if (email != null) {
+                                Log.i("KAKAO_API", "email: " + email);
+
+                            } else if (kakaoAccount.emailNeedsAgreement() == OptionalBoolean.TRUE) {
+                                // 동의 요청 후 이메일 획득 가능
+                                // 단, 선택 동의로 설정되어 있다면 서비스 이용 시나리오 상에서 반드시 필요한 경우에만 요청해야 합니다.
+
+                            } else {
+                                // 이메일 획득 불가
+                            }
+
+                            // 프로필
+                            Profile profile = kakaoAccount.getProfile();
+
+                            if (profile != null) {
+                                Log.d("KAKAO_API", "nickname: " + profile.getNickname());
+                                Log.d("KAKAO_API", "profile image: " + profile.getProfileImageUrl());
+                                Log.d("KAKAO_API", "thumbnail image: " + profile.getThumbnailImageUrl());
+
+                            } else if (kakaoAccount.profileNeedsAgreement() == OptionalBoolean.TRUE) {
+                                // 동의 요청 후 프로필 정보 획득 가능
+
+                            } else {
+                                // 프로필 획득 불가
+                            }
+
+                            Glide.with(getApplicationContext()).load( profile.getThumbnailImageUrl()).error(R.drawable.kakao_default_profile_image).into(profile_image);
+                            name.setText(profile.getNickname());
+                            kakaoEmail.setText(email);
+                        }
+                    }
+                });
     }
 }
