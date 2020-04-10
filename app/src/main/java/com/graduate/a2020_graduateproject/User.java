55eab9a0d4a6b19e137400adc15757eda4f5db23
sylee0129 <369256@naver.com>
2020-04-10 17:20:12 +0900

    firebase 사용 + 안쓰는 액티비티 정리

---
+++ app/src/main/java/com/graduate/a2020_graduateproject/User.java
@@ -0,0 +1,41 @@
+package com.graduate.a2020_graduateproject;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@IgnoreExtraProperties
+public class User {
+
+    public Long id;
+    public String name;
+    public String email;
+    public String thumbnail;
+
+    //private FirebaseDatabase mPostReference;
+
+    public User (Long id, String name, String email, String thumbnail){
+
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.thumbnail = thumbnail;
+
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("id", id);
+        result.put("name", name);
+        result.put("email", email);
+        result.put("thumbnail", thumbnail);
+        return result;
+    }
+
+
+}
