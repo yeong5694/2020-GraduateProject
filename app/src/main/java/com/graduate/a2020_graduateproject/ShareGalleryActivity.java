8d9eccf055a42b012e66554439da1dcb819e4ed8
yeawonlee <wwon525@naver.com>
2020-04-13 15:43:43 +0900

    핸드폰 저장소에서 사진을 선택하여 여행 방 갤러리에 보이기, Firebase Cloud Storage에 선택한 사진 업로드

---
+++ app/src/main/java/com/graduate/a2020_graduateproject/ShareGalleryActivity.java
@@ -0,0 +1,284 @@
+package com.graduate.a2020_graduateproject;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+public class ShareGalleryActivity extends AppCompatActivity {
+
+    /*
+    public final static int REQUEST_CODE = 1;
+    public static ArrayList<String> selectedPhotos = new ArrayList<>();*/
+
+    //private static final int REQUEST_CODE_CHOOSE = 23;  // define request code constants
+    //private static final int REQUEST_OPEN_RESULT_CODE = 0;
+    //private static final int REQUEST_CODE = 0;
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private FloatingActionButton uploadButton;
+    private ImageView imageView;
+    private ProgressBar progressBar;
+    private Button firebaseUploadButton;
+
+    private Uri imageUri;
+
+    private StorageReference storageReference;
+    private DatabaseReference rootReference;
+    private DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_gallery);
+
+        //requirePermission();    // 권한 체크
+
+        /*
+        // 카메라 버튼
+        Button button = findViewById(R.id.camera_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean camera = ContextCompat.checkSelfPermission(view.getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+
+                boolean write = ContextCompat.checkSelfPermission(view.getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+
+                if(camera && write) {
+                    // 사진찍는 인텐트
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, 0);
+                }
+                else {
+                    Toast.makeText(ShareGalleryActivity.this, "카메라 권한 및 쓰기 권한을 허용해주세요", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });*/
+
+        imageView = findViewById(R.id.selected_imageView);
+        progressBar = findViewById(R.id.progress_bar);
+
+        /*
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_OPEN_RESULT_CODE);*/
+
+        /*
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);*/
+
+        // Storage에 uploads 폴더 만듦
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
+        rootReference = FirebaseDatabase.getInstance().getReference("SharingTrips");
+        databaseReference = rootReference;
+
+        // Firebase에 업로드 버튼
+        firebaseUploadButton = findViewById(R.id.upload_button);
+        firebaseUploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadFile();
+            }
+        });
+
+        // 갤러리에서 사진 업로드 버튼
+        uploadButton = findViewById(R.id.uploadFab);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                //startActivityForResult(intent, REQUEST_CODE);
+                startActivityForResult(intent, PICK_IMAGE_REQUEST);
+                /*
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_OPEN_RESULT_CODE);*/
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imageUri = data.getData();
+
+            Glide.with(this)
+                    .load(imageUri)
+                    .into(imageView);
+        }
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cr.getType(uri));
+    }
+
+    private void uploadFile() {
+        if(imageUri != null) {
+            StorageReference fileReference = storageReference.child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+
+            fileReference.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressBar.setProgress(0);
+                                }
+                            }, 500);
+
+                            Toast.makeText(ShareGalleryActivity.this, "Upload successful", Toast.LENGTH_SHORT).show();
+                            Upload upload = new Upload(taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
+                            //Upload upload = new Upload(taskSnapshot.getStorage().getDownloadUrl().toString());
+                            String uploadId = databaseReference.push().getKey();
+                            databaseReference.child(uploadId).setValue(upload);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(ShareGalleryActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            progressBar.setProgress((int) progress);
+                        }
+                    });
+        }
+        else {
+            Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /*
+    void requirePermission() {
+
+        String[] permissions = new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        ArrayList<String> listPermissionsNeeded = new ArrayList<>();
+
+        // 필수 권한 두 가지(CAMERA와 WRITE_EXTERNAL_STORAGE) 확인. 권한이 허가되었는지 체크
+        for(String permission : permissions) {
+            // 권한이 허가가 안 됐을 경우 요청할 권한을 추가
+            if(ContextCompat.checkSelfPermission(this,permission) == PackageManager.PERMISSION_DENIED) {
+                listPermissionsNeeded.add(permission);  // permission 배열에 추가
+            }
+        }
+
+        // 이제 데이터가 있을 경우
+        if(!listPermissionsNeeded.isEmpty()) {
+            // 권한 요청 // listPermissionsNeeded이 한꺼번에 돌아가서 퍼미션 크기만큼 요청, 즉 두 개의 퍼미션을 요청
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]),1);
+        }
+    }*/
+
+    /*
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);    //?
+        if (requestCode == REQUEST_OPEN_RESULT_CODE && resultCode == RESULT_OK) {
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+
+                try {
+                    Bitmap bitmap = getBitmapFromUri(uri);
+                    imageView.setImageBitmap(bitmap);
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Glide.with(this)
+                        .load(uri)
+                        .into(imageView);
+            }
+        }
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap bitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+
+        return bitmap;
+    }*/
+
+    /*
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(resultData.getData());
+
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+
+                    imageView.setImageBitmap(img);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
+        }
+    }*/
+}
