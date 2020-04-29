64fb7f789fd1883df5b839cbaf30209b5124f1bd
yeawonlee <wwon525@naver.com>
2020-04-29 13:13:18 +0900

    외부저장소 공용 영역 접근 권한 체크 및 갤러리 폴더 만들기(안드로이드 Q에서 적용 안 됨)

--- app/src/main/java/com/graduate/a2020_graduateproject/ShareGalleryActivity.java
+++ app/src/main/java/com/graduate/a2020_graduateproject/ShareGalleryActivity.java
@@ -14,8 +14,10 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
@@ -41,10 +43,15 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 import java.io.FileDescriptor;
+import java.io.FileFilter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOError;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class ShareGalleryActivity extends AppCompatActivity {
 
@@ -62,6 +69,8 @@ public class ShareGalleryActivity extends AppCompatActivity {
     private ImageView imageView;
     private ProgressBar progressBar;
     private Button firebaseUploadButton;
+    private Button galleryFolderCreateButton;
+    private OutputStream outputStream;
 
     private Uri imageUri;
 
@@ -97,20 +106,65 @@ public class ShareGalleryActivity extends AppCompatActivity {
             }
         });*/
 
+        // 읽기 권한 체크
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1);
+            }
+        }
+
+        /*
+        // 외부저장소
+        if(isExternalStorageReadable() && isExternalStorageWritable()) {
+            accessExternalStorage();
+        }*/
+
         imageView = findViewById(R.id.selected_imageView);
         progressBar = findViewById(R.id.progress_bar);
+        firebaseUploadButton = findViewById(R.id.upload_button);
+        galleryFolderCreateButton = findViewById(R.id.createFolder_button);
 
-        /*
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("image/*");
-        startActivityForResult(intent, REQUEST_OPEN_RESULT_CODE);*/
+        galleryFolderCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if(isExternalStorageReadable() && isExternalStorageWritable()) {
+                //accessExternalStorage();
+                //}
 
-        /*
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent, PICK_IMAGE_REQUEST);*/
+                isExternalStorageWritable();
+
+                /*
+                BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+                Bitmap bitmap = drawable.getBitmap();
+
+                File filePath = Environment.getExternalStorageDirectory();
+                File dir = new File(filePath.getAbsolutePath() + "/SharingTrips");
+                dir.mkdir();
+                File file = new File(dir, System.currentTimeMillis() + ".jpg");
+                try {
+                    outputStream = new FileOutputStream(file);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                Toast.makeText(getApplicationContext(), "Image save", Toast.LENGTH_SHORT).show();
+                try {
+                    outputStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }*/
+            }
+        });
+
 
         // Storage에 uploads 폴더 만듦
         storageReference = FirebaseStorage.getInstance().getReference("uploads");
@@ -118,7 +172,6 @@ public class ShareGalleryActivity extends AppCompatActivity {
         databaseReference = rootReference;
 
         // Firebase에 업로드 버튼
-        firebaseUploadButton = findViewById(R.id.upload_button);
         firebaseUploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -143,6 +196,49 @@ public class ShareGalleryActivity extends AppCompatActivity {
                 startActivityForResult(intent, REQUEST_OPEN_RESULT_CODE);*/
             }
         });
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            accessExternalStorage();
+
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void accessExternalStorage() {
+        String folderPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/SharingTrips";
+        //String folderPath = getExternalFilesDir(Environment.DIRECTORY_PICTURES) + "/SharingTrips";
+
+        File galleryFolder = new File(folderPath);
+        if(!galleryFolder.exists()) {
+            galleryFolder.mkdir();
+        }
+
+
+        /*
+        File f = new File(FolderPath);
+        File[] files = f.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.getName().toLowerCase(Locale.US).endsWith(".jpg"); //확장자
+            }
+        });*/
     }
 
     @Override
