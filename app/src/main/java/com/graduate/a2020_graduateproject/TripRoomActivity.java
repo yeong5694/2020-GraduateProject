298c743e2ad9a982578a4f3d618b3e48c63b7913
yeawonlee <wwon525@naver.com>
2020-05-30 13:03:03 +0900

    Firebase Storage에 저장된 이미지 Firebase Realtime Database의 해당 여행방 gallery_list 아래 저장하기 + 현재 여행방 갤러리 RecyclerView로 변형하면서 에러난 부분 수정 중

--- app/src/main/java/com/graduate/a2020_graduateproject/TripRoomActivity.java
+++ app/src/main/java/com/graduate/a2020_graduateproject/TripRoomActivity.java
@@ -41,7 +41,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class TripRoomActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
+public class TripRoomActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Toolbar toolbar;
 
@@ -211,6 +211,8 @@ public class TripRoomActivity extends AppCompatActivity  implements NavigationVi
         }
         else if(id == R.id.gallery) {
             Intent intent = new Intent(TripRoomActivity.this, ShareGalleryActivity.class);
+            intent.putExtra("selected_room_name", selected_room_name);
+            intent.putExtra("selected_room_id", selected_room_id);
             startActivity(intent);
         }
         else if(id == R.id.exit){
