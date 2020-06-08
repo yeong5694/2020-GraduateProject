6b9486841e289fb04bb81de6f7a475c3a6b8ddf8
yeawonlee <wwon525@naver.com>
2020-06-09 00:03:33 +0900

    여행방 갤러리 수정중

--- app/src/main/java/com/graduate/a2020_graduateproject/UploadAdapter.java
+++ app/src/main/java/com/graduate/a2020_graduateproject/UploadAdapter.java
@@ -1,5 +1,8 @@
 package com.graduate.a2020_graduateproject;
 
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -54,29 +57,29 @@ public class UploadAdapter extends RecyclerView.Adapter<UploadAdapter.UploadView
             super(itemView);
 
             image = itemView.findViewById(R.id.image);
-/*
+
             // 아이템 클릭 이벤트 처리
-            image.setOnClickListener(new View.OnClickListener() {
+            itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-
+/*
                     int position = getAdapterPosition();
                     if(position != RecyclerView.NO_POSITION) {
                         RecyclerView item = uploadItems
                     }
-/*
-                Context context = v.getContext();
-                Log.v("gallery", "context = " + v.getContext());
+*/
+                    Context context = v.getContext();
+                    Log.v("gallery", "context = " + v.getContext());
 
-                Intent intent = new Intent(context, GalleryImageViewerActivity.class);
+                    Intent intent = new Intent(context, GalleryImageViewerActivity.class);
 
-                intent.putExtra("clicked image", itemView.toString());
-                Log.v("gallery", "image = " + itemView.toString());
+                    intent.putExtra("clicked image", itemView.toString());
+                    Log.v("gallery", "image = " + itemView.toString());
 
-                context.startActivity(intent);
-* /
+                    context.startActivity(intent);
                 }
-            });*/
+            });
         }
+
     }
 }
