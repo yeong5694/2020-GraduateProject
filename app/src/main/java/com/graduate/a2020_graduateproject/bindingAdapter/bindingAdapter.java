03c8b11d75352edc287d4e0b0674e4423618bb39
jeongmin <jmlee2232@gmail.com>
2020-06-09 01:23:00 +0900

    schedule day 바인딩

--- app/src/main/java/com/graduate/a2020_graduateproject/bindingAdapter/bindingAdapter.java
+++ app/src/main/java/com/graduate/a2020_graduateproject/bindingAdapter/bindingAdapter.java
@@ -5,6 +5,7 @@ import android.widget.TextView;
 
 import androidx.databinding.BindingAdapter;
 
+import com.graduate.a2020_graduateproject.Schedule;
 import com.graduate.a2020_graduateproject.utils.DateFormat;
 
 import java.util.Calendar;
@@ -30,6 +31,18 @@ public class bindingAdapter {
             if (calendar != null) {
                 GregorianCalendar gregorianCalendar = new GregorianCalendar(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 0, 0, 0);
                 view.setText(DateFormat.getDate(gregorianCalendar.getTimeInMillis(), DateFormat.DAY_FORMAT));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @BindingAdapter({"setScheduleDayText"})
+    public static void setScheduleDayText(TextView view, Schedule schedule) {
+        try {
+            if (schedule != null) {
+                long day = Long.parseLong(schedule.getDay());
+                view.setText(DateFormat.getDate(day,DateFormat.SCHEDULE_DAY_FORMAT));
             }
         } catch (Exception e) {
             e.printStackTrace();
